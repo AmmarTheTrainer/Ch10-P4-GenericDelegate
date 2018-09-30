bef00170afd8edbd428cc92958661c97e8adb959
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-09-30 21:10:24 +0500

    commit from class

--- Ch10-P4-GenericDelegate/Program.cs
+++ Ch10-P4-GenericDelegate/Program.cs
@@ -6,35 +6,39 @@ using System.Threading.Tasks;
 
 namespace Ch10_P4_GenericDelegate
 {
-    // This generic delegate can represnet any method
-    // returning void and taking a single parameter of type T.
-    public delegate void MyGenericDelegate<T>(T arg);
+    //// This generic delegate can represnet any method
+    //// returning void and taking a single parameter of type T.
+    //public delegate void MyGenericDelegate<T>(T arg);
 
-    public delegate void GenericSumDelegate<T>(T arg1, T arg2);
+    //public delegate void GenericSumDelegate<T>(T arg1, T arg2);
 
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("***** Generic Delegates *****\n");
-            //// Register targets.
-            //MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
-            //strTarget("Some string data");
-            //MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
-            //intTarget(9);
+            ////// Register targets.
+            ////MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            ////strTarget("Some string data");
+            ////MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            ////intTarget(9);
 
-            GenericSumDelegate<int> delSum = new GenericSumDelegate<int>(Sum);
-            delSum.Invoke(3, 3);
-            Console.WriteLine();
+            //GenericSumDelegate<int> delSum = new GenericSumDelegate<int>(Sum);
+            //delSum.Invoke(3, 3);
+            //Console.WriteLine();
 
-            GenericSumDelegate<float> delSum2 = new GenericSumDelegate<float>(Sum);
-            delSum2.Invoke(3, 3);
+            //GenericSumDelegate<float> delSum2 = new GenericSumDelegate<float>(Sum);
+            //delSum2.Invoke(3, 3);
 
-            GenericSumDelegate<double> delsum3 = new GenericSumDelegate<double>(GenericSum);
-            delsum3.Invoke(3, 3);
+            //GenericSumDelegate<double> delsum3 = new GenericSumDelegate<double>(GenericSum);
+            //delsum3.Invoke(3, 3);
 
-            GenericSumDelegate<short> delsum4 = new GenericSumDelegate<short>(GenericSum);
-            delsum4.Invoke(3, 3);
+            //GenericSumDelegate<short> delsum4 = new GenericSumDelegate<short>(GenericSum);
+            //delsum4.Invoke(3, 3);
+
+           //Action<
+
+           // Func<>
 
             Console.ReadLine();
         }
